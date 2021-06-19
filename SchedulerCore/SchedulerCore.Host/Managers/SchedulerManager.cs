3da4c428ecbb5c38a32f5beef299f7bd0cd01b0f
wenzhongxu <xuwenzhong1994@163.com>
2021-06-19 21:29:16 +0800

    scheduler core project

---
+++ SchedulerCore/SchedulerCore.Host/Managers/SchedulerManager.cs
@@ -0,0 +1,43 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulerCore.Host.Managers
+{
+    /// <summary>
+    /// 任务调度中心 单例模式
+    /// </summary>
+    public class SchedulerManager
+    {
+        private IScheduler _scheduler;
+
+        /// <summary>
+        /// 开启调度器
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> StartScheduleAsync()
+        {
+            await InitSchedulerAsync();
+
+            if (_scheduler.InStandbyMode)
+            {
+                await _scheduler.Start();
+                Console.WriteLine("任务调度已启动。");
+            }
+
+            return _scheduler.InStandbyMode;
+        }
+
+        private async Task InitSchedulerAsync()
+        {
+            if (_scheduler == null)
+            {
+                // 初始化任务调度器
+            }
+        }
+
+    }
+}
---
+++ SchedulerCore/SchedulerCore.Host/Services/HostedService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using SchedulerCore.Host.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SchedulerCore.Host.Services
+{
+    public class HostedService : IHostedService
+    {
+        private readonly SchedulerManager _schedulerManager;
+
+        public HostedService(SchedulerManager schedulerManager)
+        {
+            _schedulerManager = schedulerManager ?? throw new ArgumentNullException(nameof(schedulerManager));
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _schedulerManager.StartScheduleAsync();
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
