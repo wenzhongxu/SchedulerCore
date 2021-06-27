792f2a63722ede9c80dcde4d5387ba0765305f1c
wenzhongxu <xuwenzhong1994@163.com>
2021-06-27 21:54:18 +0800

    controller

---
+++ SchedulerCore/SchedulerCore.Host/Controllers/JobController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SchedulerCore.Host.Entities;
+using SchedulerCore.Host.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulerCore.Host.Controllers
+{
+    [ApiController]
+    [Route("api/job")]
+    public class JobController : ControllerBase
+    {
+        private readonly ILogger<JobController> _logger;
+        private SchedulerCenter _scheduler;
+
+        public JobController(ILogger<JobController> logger, SchedulerCenter scheduler)
+        {
+            _logger = logger;
+            _scheduler = scheduler;
+        }
+
+        /// <summary>
+        /// 获取所有任务
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<List<JobInfoEntity>>> GetAllJob()
+        {
+            var jobs = await _scheduler.GetAllJobAsync();
+            return Ok(jobs);
+        }
+    }
+}
