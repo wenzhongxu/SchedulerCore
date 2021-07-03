ef6b4b40178b5de98644fb139fd4da4c5fbe6066
wenzhongxu <xuwenzhong1994@163.com>
2021-07-03 21:07:32 +0800

    http job

---
+++ SchedulerCore/SchedulerCore.Host/Helpers/HttpHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SchedulerCore.Host.Helpers
+{
+    /// <summary>
+    /// HTTP请求
+    /// </summary>
+    public class HttpHelper
+    {
+        public static readonly HttpHelper Instance;
+
+        static HttpHelper()
+        {
+            Instance = new HttpHelper();
+        }
+
+        public static ConcurrentDictionary<string, HttpClient> dictionary = new ConcurrentDictionary<string, HttpClient>();
+
+        private HttpClient GetHttpClient(string url)
+        {
+            var uri = new Uri(url);
+            var key = uri.Scheme + uri.Host;
+            return dictionary.GetOrAdd(key, new HttpClient());
+        }
+
+        public async Task<HttpResponseMessage> PostAsync(string url, string jsonString, Dictionary<string, string> headers = null)
+        {
+            if (string.IsNullOrEmpty(jsonString))
+            {
+                jsonString = "{}";
+            }
+
+            StringContent content = new StringContent(jsonString);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            if (headers != null && headers.Any())
+            {
+                using HttpClient http = new HttpClient();
+                foreach (var item in headers)
+                {
+                    http.DefaultRequestHeaders.Remove(item.Key);
+                    http.DefaultRequestHeaders.TryAddWithoutValidation(item.Key, item.Value);
+                }
+                return await http.PostAsync(new Uri(url), content);
+            }
+            else
+            {
+                return await GetHttpClient(url).PostAsync(new Uri(url), content);
+            }
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url, Dictionary<string, string> headers = null)
+        {
+            if (headers != null && headers.Any())
+            {
+                using HttpClient http = new HttpClient();
+                foreach (var item in headers)
+                {
+                    http.DefaultRequestHeaders.Remove(item.Key);
+                    http.DefaultRequestHeaders.TryAddWithoutValidation(item.Key, item.Value);
+                }
+                return await http.GetAsync(url);
+            }
+            else
+            {
+                return await GetHttpClient(url).GetAsync(url);
+            }
+        }
+    }
+}
--- SchedulerCore/SchedulerCore.Host/Jobs/HttpJob.cs
+++ SchedulerCore/SchedulerCore.Host/Jobs/HttpJob.cs
@@ -1,16 +1,54 @@
-﻿using Quartz;
+﻿using Newtonsoft.Json;
+using Quartz;
+using SchedulerCore.Host.Common;
+using SchedulerCore.Host.Common.Enum;
+using SchedulerCore.Host.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace SchedulerCore.Host.Jobs
 {
     public class HttpJob : IJob
     {
-        public Task Execute(IJobExecutionContext context)
+        public async Task Execute(IJobExecutionContext context)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("开始执行http任务");
+            //获取相关参数
+            var requestUrl = context.JobDetail.JobDataMap.GetString(Constant.RequestUrl)?.ToString();
+            requestUrl = requestUrl?.IndexOf("http") == 0 ? requestUrl : "http://" + requestUrl;
+            var requestParameters = context.JobDetail.JobDataMap.GetString(Constant.RequestParameters);
+            var headerString = context.JobDetail.JobDataMap.GetString(Constant.Headers);
+            var headers = headerString != null ? JsonConvert.DeserializeObject<Dictionary<string, string>>(headerString?.Trim()) : null;
+            var requestType = (RequestTypeEnum)int.Parse(context.JobDetail.JobDataMap.GetString(Constant.RequestType));
+
+            HttpResponseMessage responseMessage = new HttpResponseMessage();
+            var http = HttpHelper.Instance;
+            switch (requestType)
+            {
+                case RequestTypeEnum.Get:
+                    responseMessage = await http.GetAsync(requestUrl, headers);
+                    break;
+                case RequestTypeEnum.Post:
+                    responseMessage = await http.PostAsync(requestUrl, requestParameters, headers);
+                    break;
+                case RequestTypeEnum.Put:
+                    break;
+                case RequestTypeEnum.Delete:
+                    break;
+                default:
+                    break;
+            }
+
+            var result = HttpUtility.HtmlEncode(await responseMessage.Content.ReadAsStringAsync());
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                context.JobDetail.JobDataMap[Constant.Exception] = result;
+            }
         }
     }
 }
